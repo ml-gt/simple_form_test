966633a6ab037133e5cb605f9680968438b57f9d
gt-sdi <gto.sdi@gmail.com>
2012-01-02 21:40:19 -0700

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleFormTest::Application.config.secret_token = 'bf854e72f74c7ed1dfc9300b1406c1da58e49cd9651d9438d46ea8aa209385d103a5a5f7be046b1b8281696ab21ad8e816fcbe41b396ed151bfb64a263abc537'
